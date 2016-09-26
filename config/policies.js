34972a40f83e29262c15d133b8d8060e4c44dc44
Luis Arce <luis.arce@jalasoft.com>
2016-09-26 13:58:18 -0400

    Generate and auth local

--- config/policies.js
+++ config/policies.js
@@ -19,6 +19,21 @@
 
 module.exports.policies = {
 
+  '*': [
+    'basicAuth',
+    'passport',
+    'sessionAuth',
+    'ModelPolicy',
+    'AuditPolicy',
+    'OwnerPolicy',
+    'PermissionPolicy',
+    'RolePolicy',
+    'CriteriaPolicy'
+  ],
+ 
+  AuthController: {
+    '*': [ 'passport' ]
+  }
   /***************************************************************************
   *                                                                          *
   * Default policy for all controllers and actions (`true` allows public     *
